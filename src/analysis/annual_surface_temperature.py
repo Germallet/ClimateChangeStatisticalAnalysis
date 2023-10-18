a4820aa1ce4a699381316cb7aaeee9ded9f785fb
Germán <germanmallet@gmail.com>
2023-10-17 23:21:53 -0300

    Annual Surface Temperature Change

--- src/analysis/annual_surface_temperature.py
+++ src/analysis/annual_surface_temperature.py
@@ -1,6 +1,18 @@
-from utils import *
+import pandas as pd
+import utils
 
-df = read_csv("Annual_Surface_Temperature_Change.csv")
-years = df.columns[10:-1]
-world_surface_temperature_change = df.loc[df["Country"] == "World"].iloc[0][10:-1]
-plot_all(years, world_surface_temperature_change, "Date", "Degree Celsius", "Annual Surface Temperature Change", "", "Annual Surface Temperature Change")
+# Read the CSV file into a DataFrame
+temp_df = utils.read_csv("Annual_Surface_Temperature_Change.csv")
+
+# Read year columns
+temp_years = pd.to_datetime(temp_df.columns[10:-1], format='F%Y')
+
+# Get world row and extract values from year columns
+temp_values = temp_df.loc[temp_df["Country"] == "World"].iloc[0][10:-1]
+
+# Generate timeserie Dataframe
+df = pd.DataFrame({"Date": temp_years, "Value": temp_values})
+df.set_index('Date', inplace=True)
+
+if __name__ == '__main__':
+    utils.plot_all(df.index, df["Value"], "Date", "Degree Celsius", "Annual Surface Temperature Change", "Annual Surface Temperature Change")
--- src/analysis/utils.py
+++ src/analysis/utils.py
@@ -1,7 +1,7 @@
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import pandas as pd
 
 DATA_DIR = os.path.dirname(os.path.realpath(__file__)) + "/../../data/"
 IMG_DIR = os.path.dirname(os.path.realpath(__file__)) + "/../../images/"
@@ -10,7 +10,7 @@ def read_csv(filename):
     df = pd.read_csv(os.path.join(DATA_DIR, filename))
     return df
 
-def plot(x_values, y_values, x_label, y_label, title, path, filename):
+def plot(x_values, y_values, x_label, y_label, title, filename, periods = 10):
     plt.figure(figsize=(12, 8))
     plt.plot(x_values, y_values)
     plt.xlabel(x_label)
@@ -18,19 +18,21 @@ def plot(x_values, y_values, x_label, y_label, title, path, filename):
     plt.ylabel(y_label)
     plt.title(title)
     if isinstance(x_values, np.ndarray):
-        plt.xticks(np.linspace(x_values.min(), x_values.max(), 10))
+        plt.xticks(np.linspace(x_values.min(), x_values.max(), periods))
+    elif isinstance(x_values, pd.DatetimeIndex):
+        plt.xticks(pd.date_range(start=x_values.min(), end=x_values.max(), periods=periods))
     else:
-        plt.xticks(np.arange(0, x_values.size, np.floor(x_values.size/10)))
+        plt.xticks(np.arange(0, x_values.size, np.floor(x_values.size/periods)))
     plt.xlim(x_values.min(), x_values.max())
     plt.grid(True)
-    plt.savefig(os.path.join(IMG_DIR, path, f"{filename}.png"))
+    plt.savefig(os.path.join(IMG_DIR, f"{filename}.png"))
     plt.show()
 
-def plot_ft(y_values, x_label, y_label, title, path, filename):
+def plot_ft(y_values, x_label, y_label, title, filename, periods = 10):
     fft = np.fft.fft(y_values)
     frequencies = np.fft.fftfreq(len(fft))
-    plot(frequencies[frequencies > 0], np.abs(fft[frequencies > 0]), x_label, y_label, title, path, filename)
+    plot(frequencies[frequencies > 0], np.abs(fft[frequencies > 0]), x_label, y_label, title, filename, periods)
 
-def plot_all(x_values, y_values, x_label, y_label, title, path, filename):
-    plot(x_values, y_values, x_label, y_label, title, path, filename)
-    plot_ft(y_values, "Frequency", "Amplitude", f"{title} (FT)", path, f"{filename} FT")
+def plot_all(x_values, y_values, x_label, y_label, title, filename, periods = 10):
+    plot(x_values, y_values, x_label, y_label, title, filename, periods)
+    plot_ft(y_values, "Frequency", "Amplitude", f"{title} (FT)", f"{filename} FT")
