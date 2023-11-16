d0b191bb30c2c807bcaa1236664af10b3de76d9f
Nahuel Gil Peña <nahuelgp2000@hotmail.com>
2023-11-16 19:33:02 -0300

    # Fixed forecasting

--- src/analysis/forecasting.py
+++ src/analysis/forecasting.py
@@ -11,7 +11,9 @@ import os
 
 IMG_DIR = os.path.dirname(os.path.realpath(__file__)) + "/../../images/forecasting/"
 
-temperature_df = surface_temperature_change.dataframe() # Data from 1850 to 2023
+temperature_df = surface_temperature_change.dataframe(to_year=2022) # Data from 1850 to 2023
+# Date filter
+temperature_df = temperature_df.resample("Y").mean(numeric_only=True)
 
 print("PRE-Diff()")
 print("Temperature DF")
@@ -20,7 +22,7 @@ print(adfuller(temperature_df)[1])
 print("KPSS")
 print(kpss(temperature_df)[1])
 
-window_size = 5  # Adjust the window size as needed
+window_size = 12  # Adjust the window size as needed
 
 aux = temperature_df.diff().rolling(window=window_size, min_periods=0).mean()
 temperature_df = temperature_df.diff().fillna(aux)
@@ -44,11 +46,11 @@ plt.savefig(os.path.join(IMG_DIR, "Monthly Temperature Time Series.png"))
 plt.clf()
 
 # Split the data into training and testing sets
-train_size = int(len(ts) * 0.8)
+train_size = int(len(ts) * 0.9)
 train, test = ts[:train_size + 1], ts[train_size:]
 
 # Fit an ARIMA model
-order = (15, 0, 15)  # Replace with appropriate order based on your data and analysis
+order = (3, 0, 5)  # Replace with appropriate order based on your data and analysis
 model = ARIMA(train, order=order)
 fit_model = model.fit()
 
@@ -56,7 +58,7 @@ fit_model = model.fit()
 print("Test Len", len(test))
 forecast_steps = len(test)
 forecast = fit_model.get_forecast(steps=forecast_steps)
-forecast_index = pd.date_range(start=train.index[-2], periods=forecast_steps, freq='M')
+forecast_index = pd.date_range(start=train.index[-1], periods=forecast_steps, freq='Y')
 
 # Plot the training data, testing data, and the forecast
 plt.figure(figsize=(12, 8))
@@ -67,3 +69,5 @@ plt.title('ARIMA Model Forecasting')
 plt.legend()
 plt.savefig(os.path.join(IMG_DIR, "Monthly Temperature Forecasting.png"))
 plt.clf()
+
+print(forecast.predicted_mean.value_counts())
